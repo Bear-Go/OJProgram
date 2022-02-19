590edacbe19fda9aec358d9bf7874eed40667aaf
Bear-Go <jerry2262@outlook.com>
2022-02-19 21:29:26 +0800

    2022/2/19 update

---
+++ Leetcode/T969.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> arr;
+
+int main() {
+
+    int x;
+    while (cin >> x) {
+        arr.push_back(x);
+    }
+    vector<int> ans;
+
+    vector<int> s(arr);
+    sort(s.begin(), s.end());
+
+    for (int i = s.size() - 1; i >= 1; --i) {
+        for (int j = 0; j <= i; ++j) {
+            if (arr[j] == s[i]) {
+                ans.push_back(j + 1);
+                ans.push_back(i + 1);
+                reverse(arr.begin(), arr.begin() + j + 1);
+                reverse(arr.begin(), arr.begin() + i + 1);
+                break;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < ans.size(); ++i) {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
