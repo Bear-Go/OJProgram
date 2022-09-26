bd00846b157519d2c1b0868ae74703572b02c090
Bear-Go <jerry2262@outlook.com>
2022-09-26 22:02:43 +0800

    2022/9/26

---
+++ Leetcode/M1719.cpp
@@ -0,0 +1,16 @@
+// 面试题 17.19. 消失的两个数字
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> missingTwo(vector<int>& nums) {
+        
+    }
+};
+
+int main() {
+
+}
