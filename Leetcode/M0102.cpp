b1ee213527df36450abc2bff580c280427fe37e7
Bear-Go <jerry2262@outlook.com>
2022-09-27 09:07:37 +0800

    2022/9/27

---
+++ Leetcode/M0102.cpp
@@ -0,0 +1,25 @@
+// 面试题 01.02. 判定是否互为字符重排
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool CheckPermutation(string s1, string s2) {
+
+    }
+};
+
+void test1() {
+
+}
+
+void test2() {
+
+}
+
+int main() {
+    test1();
+    test2();
+}
