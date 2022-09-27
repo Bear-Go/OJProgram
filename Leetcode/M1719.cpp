026955423736cefc73e3c123315b3ac3af45c60a
Bear-Go <jerry2262@outlook.com>
2022-09-27 09:03:43 +0800

    2022/9/26

--- Leetcode/M1719.cpp
+++ Leetcode/M1719.cpp
@@ -7,10 +7,45 @@ using namespace std;
 class Solution {
 public:
     vector<int> missingTwo(vector<int>& nums) {
-        
+        long long size = (long long)nums.size() + 2;
+        long long sum = size * (size + 1) / 2;
+        long long square_sum = size * (size + 1) * (2 * size + 1) / 6;
+        for (auto x : nums) {
+            sum -= x;
+            square_sum -= x * x;
+        }
+        long long diff = (2 * square_sum - sum * sum);
+        vector<int> ans;
+        ans.push_back((sum - sqrt(2 * square_sum - sum * sum)) / 2);
+        ans.push_back((sum + sqrt(2 * square_sum - sum * sum)) / 2);
+        return ans;
     }
 };
 
-int main() {
+void test1() {
+    vector<int> input;
+    input.push_back(1);
+    Solution p;
+    auto output = p.missingTwo(input);
+    for (auto x : output) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
 
+void test2() {
+    vector<int> input;
+    input.push_back(2);
+    input.push_back(3);
+    Solution p;
+    auto output = p.missingTwo(input);
+    for (auto x : output) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    test1();
+    test2();
 }
