d76cfe147c1c9f8d0234baeed3c07238e2b6b732
Bear-Go <jerry2262@outlook.com>
2022-09-27 09:28:32 +0800

    2022/9/27

--- Leetcode/M0102.cpp
+++ Leetcode/M0102.cpp
@@ -7,12 +7,18 @@ using namespace std;
 class Solution {
 public:
     bool CheckPermutation(string s1, string s2) {
-
+        if (s1.length() != s2.length()) return false;
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
+        for (int i = 0; i < s1.length(); ++i) {
+            if (s1[i] != s2[i]) return false;
+        }
+        return true;
     }
 };
 
 void test1() {
-
+    
 }
 
 void test2() {
