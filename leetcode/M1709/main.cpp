0291aed78133c9156c713e5d3555370895bb614b
Bear-Go <jerry2262@outlook.com>
2022-09-28 22:42:41 +0800

    2022/9/28 new

---
+++ leetcode/M1709/main.cpp
@@ -0,0 +1,19 @@
+// 面试题 17.09. 第 k 个数
+
+// x = 3 ^ a * 5 ^ b * 7 ^ c (a,b,c >= 0)
+// lg(x) = alg3 + blg5 + clg7 (a,b,c >= 0)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int getKthMagicNumber(int k) {
+
+    }
+};
+
+int main() {
+    Solution p;
+    cout << p.getKthMagicNumber(5) << endl;
+}
