da229c0294115cfe70fc7274f4ab06d51bac7f19
Bear-Go <jerry2262@outlook.com>
2022-09-29 11:11:57 +0800

    2022/9/28

--- leetcode/M1709/main.cpp
+++ leetcode/M1709/main.cpp
@@ -1,7 +1,7 @@
 // 面试题 17.09. 第 k 个数
 
 // x = 3 ^ a * 5 ^ b * 7 ^ c (a,b,c >= 0)
-// lg(x) = alg3 + blg5 + clg7 (a,b,c >= 0)
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -9,11 +9,21 @@ using namespace std;
 class Solution {
 public:
     int getKthMagicNumber(int k) {
-
+        int vec[k];
+        vec[0] = 1;
+        int a, b, c;
+        a = b = c = 0;
+        for (int i = 1; i < k; ++i) {
+            vec[i] = min(vec[a]*3, min(vec[b]*5, vec[c]*7));
+            a += vec[i] == vec[a]*3;
+            b += vec[i] == vec[b]*5;
+            c += vec[i] == vec[c]*7;
+        }
+        return vec[k-1];
     }
 };
 
 int main() {
     Solution p;
-    cout << p.getKthMagicNumber(5) << endl;
+    cout << p.getKthMagicNumber(15) << endl;
 }
